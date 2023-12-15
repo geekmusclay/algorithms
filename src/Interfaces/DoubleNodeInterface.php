ff6550c27118b4a4ad2e541ef5fd72107a01b9d8
cedric <cedric@vision-si.com>
2023-12-15 12:11:12 +0100

    master: added double node for lists

---
+++ src/Interfaces/DoubleNodeInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Geekmusclay\Algorithms\Interfaces;
+
+interface NodeInterface
+{
+	public function __construct(mixed $value, ?NodeInterface $next = null);
+
+	public function getValue(): mixed;
+
+	public function setValue(mixed $value): self;
+
+	public function getNext(): ?NodeInterface;
+
+	public function setNext(?NodeInterface $next): self;
+
+    public function getPrevious(): ?NodeInterface;
+
+	public function setPrevious(?NodeInterface $previous): self;
+}
